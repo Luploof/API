e262fa9e6e6a7fc84e793766321b5ab3b3d5bd36
Luploof <139908074+Luploof@users.noreply.github.com>
2024-12-21 16:22:10 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+    },
 })
